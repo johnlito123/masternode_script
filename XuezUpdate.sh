f7e4d2716eef02596be840fefdeab51d3766b1e1
God-Father <33990385+johnlito123@users.noreply.github.com>
2018-09-11 12:46:29 +0100

    Update XuezUpdate.sh

--- XuezUpdate.sh
+++ XuezUpdate.sh
@@ -51,28 +51,20 @@ echo "**************************************************************************
 echo ""
 echo ""
 echo ""
-echo "Start Installation? [Y/N]"
-read USETUP
-	if 
-	[[ $USETUP =~ "y" ]] || [[$USETUP =~ "Y" ]] ; then
+
 sudo su -c "echo 'deb http://deb.torproject.org/torproject.org '$(lsb_release -c | cut -f2)' main' > /etc/apt/sources.list.d/torproject.list"
-	gpg --keyserver keys.gnupg.net --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89
-	gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
-	sudo apt-get update
-	sudo apt-get install tor deb.torproject.org-keyring
-	sudo usermod -a -G debian-tor $(whoami)
+gpg --keyserver keys.gnupg.net --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89
+gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install tor deb.torproject.org-keyring
+sudo usermod -a -G debian-tor $(whoami)
+sudo sed -i 's/#ControlPort 9051/ControlPort 9051/g' /etc/tor/torrc
+sudo sed -i 's/#CookieAuthentication 1/CookieAuthentication 1/g' /etc/tor/torrc
+sudo su -c "echo 'CookieAuthFileGroupReadable 1' >> /etc/tor/torrc"
+sudo su -c "echo 'LongLivedPorts 9033' >> /etc/tor/torrc"
+sudo systemctl restart tor.service
 
-	sudo sed -i 's/#ControlPort 9051/ControlPort 9051/g' /etc/tor/torrc
-	sudo sed -i 's/#CookieAuthentication 1/CookieAuthentication 1/g' /etc/tor/torrc 
-	sudo su -c "echo 'CookieAuthFileGroupReadable 1' >> /etc/tor/torrc"
-	sudo su -c "echo 'LongLivedPorts 9033' >> /etc/tor/torrc"
-	sudo systemctl restart tor.service
-fi
-
-echo "Do you want to configure your VPS with the recommended Xuez settings? [y/n]"
-read DOSETUP
-	if 
-	[[ $DOSETUP =~ "y" ]] || [[$DOSETUP =~ "Y" ]] ; then
+	
 	sudo apt-get update
 	sudo apt-get -y upgrade
 	sudo apt-get -y dist-upgrade
@@ -95,15 +87,10 @@ tar -xvzf xuez-linux-cli-10110.tgz
 rm xuez-linux-cli-10110.tgz									
 sudo su -c "echo -e 'listenonion=1' >> $CONF_DIR/$CONF_FILE"
 echo "" >> $CONF_DIR/$CONF_FILE && echo "listenonion=1"  >> $CONF_DIR/$CONF_FILE
-fi
 
 echo "Masternode Configuration"
 echo "Your recognised IP address is:"
 sudo hostname -I 
-echo "Is this the IP you wish to use for MasterNode ? [y/n] , followed by [ENTER]"
-read IPDEFAULT
-	if
-	[[ $IPDEFAULT =~ "y" ]] || [[$IPDEFAULT =~ "Y" ]] ; then
 	echo ""
 	echo "We are using your default IP address"
 	echo "Enter masternode private key for node, followed by [ENTER]: $ALIAS"
